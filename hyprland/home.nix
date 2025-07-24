ff4dd6901fc572293d9a3f5a539ea4cc2a8d880c
Max Dexheimer <maxdexh03@gmail.com>
2025-07-24 17:43:07 +0200

    Default apps

--- hyprland/home.nix
+++ hyprland/home.nix
@@ -44,6 +44,18 @@
     enable = true;
     defaultApplications = {
       "application/pdf" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+
       # TODO: Add desktop entry that opens a new kitty tab with nvim for text
     };
   };
--- hyprland/system.nix
+++ hyprland/system.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.hyprland = {
@@ -6,4 +6,10 @@
     withUWSM = true;
     xwayland.enable = true;
   };
+
+  # HACK: This fixes dolphin not seeing file associations in hyprland. 
+  #       this file is created by kbuildsycoca5 (also overwrites mimeapps.list)
+  #       https://discourse.nixos.org/t/dolphin-does-not-have-mime-associations/48985/7
+  environment.etc."xdg/menus/applications.menu".source =
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 }
