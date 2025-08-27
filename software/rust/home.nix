3b2cb0b290512dd7a5dffd278fca99c914f216eb
Max Dexheimer <maxdexh03@gmail.com>
2025-08-27 16:23:57 +0200

    Don't write rustup/settings.toml

--- software/rust/home.nix
+++ software/rust/home.nix
@@ -9,9 +9,4 @@
     RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
   };
   home.sessionPath = ["${config.xdg.dataHome}/cargo/bin"];
-
-  xdg.dataFile = {
-    # use x86 stable as default
-    "rustup/settings.toml".source = ./settings.toml;
-  };
 }
