c11f5b63aedda6cf70ffe24966d7bad2ebb6123f
Max Dexheimer <maxdexh03@gmail.com>
2025-10-17 16:25:47 +0200

    Fix qt themes

--- software/desktop/home.nix
+++ software/desktop/home.nix
@@ -4,9 +4,23 @@
   lib,
   ...
 }: {
+  home.packages = with pkgs; [qt6ct];
   systemd.user.sessionVariables = {
     # gtk.theme is dysfunctional, but this works nicely, except that it still has window decorations.
     GTK_THEME = "Breeze:dark"; # or: "Adwaita:dark"
+
+    # Configure via qt6ct (qt6 config tool):
+    # - Appearance
+    #   - Style: Breeze
+    #   - Color Scheme: Style's Colors (set to 'darker' first because palettes are not set and remain white otherwise)
+    #   - Standard Dialogs: KDE
+    # - Fonts
+    #   - General, Fixed Width: Noto Sans 10
+    # - Icon Theme
+    #   - Breeze Dark
+    # - Interface
+    #   - Dialog Buttons Layout, Keyboard Scheme: KDE
+    QT_QPA_PLATFORMTHEME = "qt6ct";
 
     # Make electron apps use wayland directly rather than running through xwayland
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
