547f7e7d6e647fb1a2a0cbde88762eb510f18acb
Max <maxdexh03@gmail.com>
2025-10-29 15:34:25 +0100

    Comments

--- shared/desktop/kde/hm.nix
+++ shared/desktop/kde/hm.nix
@@ -1,7 +1,3 @@
 {...}: {
-  # TODO: https://github.com/AlexNabokikh/nix-config/blob/master/modules/home-manager/desktop/kde/default.nix
-
-  # home.file.".config/kde.org".source = ./kde.org;
-  # home.file.".config/kdedefaults".source = ./kdedefaults;
-  # home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source = ./plasma-org.kde.plasma.desktop-appletsrc;
+  # TODO: https://github.com/nix-community/plasma-manager
 }
