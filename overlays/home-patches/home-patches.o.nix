33c51ecf49b460e6b75863a1e2602c2a237d3a95
Max Dexheimer <maxdexh03@gmail.com>
2025-11-19 14:05:57 +0100

    Use overrideaAttrs instead of symlinkJoin to patch HOME

--- overlays/home-patches/home-patches.o.nix
+++ overlays/home-patches/home-patches.o.nix
@@ -11,8 +11,8 @@ final: prev: let
 
   # https://github.com/NixOS/nixpkgs/blob/1786e50c5eeaffc53ae7bd71f95daefe4f2ebe1c/pkgs/build-support/setup-hooks/make-wrapper.sh#L24
   override_patch_home = {
-    data_dir,
-    package,
+    name,
+    package ? prev.${name},
   }: package.overrideAttrs (prevAttrs: {
     nativeBuildInputs = prevAttrs.nativeBuildInputs or [] ++ [prev.makeWrapper];
     buildCommand = /* bash */ ''
@@ -23,7 +23,7 @@ final: prev: let
       #     HOME DIR PATCH     #
       ##########################
       for f in $out/bin/*; do
-        wrapProgram "$f" --run ${patch_home_wrap_program_run data_dir}
+        wrapProgram "$f" --run ${patch_home_wrap_program_run name}
       done
       ##########################
       #   END HOME DIR PATCH   #
@@ -31,37 +31,10 @@ final: prev: let
     '';
   });
 
-  # https://wiki.nixos.org/wiki/Nix_Cookbook#Wrapping_packages
-  join_patch_home = {
-    name,
-    pkg ? prev.${name},
-  }: prev.runCommand name {} ''
-    mkdir $out
-    # Link every top-level folder the package
-    ln -s ${pkg}/* $out
-    # Except the bin folder
-    rm $out/bin
-    mkdir $out/bin
-
-    # We create a patched wrapper for every binary
-    for f in ${pkg}/bin/*; do
-      local fOut="$out/bin/''${f#"${pkg}/bin/"}"
-      cat <<EOF >"$fOut"
-    if [[ -n "\$XDG_DATA_HOME" ]]; then
-      export HOME="\$XDG_DATA_HOME/${name}"
-    fi
-    exec "$f" $@
-    EOF
-      chmod +x "$fOut"
-    done
-  '';
 in {
-  lunar-client = join_patch_home {name = "lunar-client";};
-  thunderbird = join_patch_home {name = "thunderbird";};
+  lunar-client = override_patch_home {name = "lunar-client";};
+  thunderbird = override_patch_home {name = "thunderbird";};
 
   # TODO: Change default download dir via policy
-  firefox = override_patch_home {
-    data_dir = "firefox";
-    package = prev.firefox;
-  };
+  firefox = override_patch_home {name = "firefox";};
 }
