a7bd9b78a7184d250d4ca0dc454ad2d7813908c4
Max Dexheimer <maxdexh03@gmail.com>
2025-11-22 11:16:27 +0100

    Fix annoying messages from jdtls

--- shared/cli/nvim/lua/plugins/langs/java.lua
+++ shared/cli/nvim/lua/plugins/langs/java.lua
@@ -8,6 +8,20 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.lsp.config("jdtls", {
    root_markers = { "pom.xml" },
+   -- https://www.reddit.com/r/neovim/comments/1jbzqp5/comment/mi4ox6v/
+   handlers = {
+      ["$/progress"] = function(err, result, ctx)
+         local msg = result.value.message
+         if msg and msg:sub(1, 18) == "Validate documents" then
+            return
+         end
+         if msg and msg:sub(1, 19) == "Publish Diagnostics" then
+            return
+         end
+         -- pass through to normal handler
+         vim.lsp.handlers["$/progress"](err, result, ctx)
+      end,
+   },
 })
 
 return {
