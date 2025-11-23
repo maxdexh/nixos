200d4487b696efbf463761954e96a098ba7a2080
Max Dexheimer <maxdexh03@gmail.com>
2025-11-23 12:43:26 +0100

    Mathematica

---
+++ shared/langs/devshells/wolfram/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake for java development";
+
+  inputs = {
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "java-dev";
+
+          buildInputs = [
+          ];
+        };
+      }
+    );
+}
