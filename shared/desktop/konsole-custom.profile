339d9495b01e94cf479ea966c8c419367023e31f
Max Dexheimer <maxdexh03@gmail.com>
2025-11-25 10:14:08 +0100

    Add konsole profile

---
+++ shared/desktop/konsole-custom.profile
@@ -0,0 +1,23 @@
+[Appearance]
+BoldIntense=true
+ColorScheme=Campbell
+Font=CaskaydiaMono Nerd Font,11,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+UseFontLineChararacters=false
+
+[General]
+AlternatingBars=0
+Command=fish
+ErrorBackground=0
+ErrorBars=0
+Name=fish
+Parent=FALLBACK/
+SemanticInputClick=false
+
+[Scrolling]
+HighlightScrolledLines=false
+HistoryMode=1
+ReflowLines=true
+ScrollBarPosition=2
+
+[Terminal Features]
+BellMode=3
--- shared/desktop/misc.nix
+++ shared/desktop/misc.nix
@@ -33,10 +33,13 @@
 
     # HACK: Hotfix for cursor theme not working in steam.
     # https://github.com/ValveSoftware/steam-for-linux/issues/11484#issuecomment-3437303820
-    home.file.".local/share/icons/default" = lib.mkIf config.custom.host.fullDesktop {
+    xdg.dataFile."icons/default" = lib.mkIf config.custom.host.fullDesktop {
       source = "${pkgs.kdePackages.breeze}/share/icons/breeze_cursors/";
       recursive = true;
     };
+
+    # Basic profile with better appearance and using fish shell
+    xdg.dataFile."konsole/custom.profile".source = config.lib.custom.mkNixConfigSymlink ./konsole-custom.profile;
 
     custom.sessionVars = lib.mkIf config.custom.host.fullDesktop {
       # gtk.theme is dysfunctional, but this works nicely, except that it still has window decorations.
