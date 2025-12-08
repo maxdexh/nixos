e38ef1fc06a717af43d3593cca0c45058faad235
Max Dexheimer <maxdexh03@gmail.com>
2025-12-08 20:28:45 +0100

    Fix missing experimental flags

--- shared/base.nix
+++ shared/base.nix
@@ -4,6 +4,8 @@
   custom,
   ...
 }: ctx.os.set {
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
