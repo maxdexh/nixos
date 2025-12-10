b4777283f4fdb4572ded26f5efaa9e7f268902f5
Max Dexheimer <maxdexh03@gmail.com>
2025-12-10 10:02:43 +0100

    Refactor

--- flake.nix
+++ flake.nix
@@ -20,24 +20,22 @@
 
   outputs = inputs: let
     hosts = builtins.attrValues (import ./hosts inputs);
-    alejandra_overlay = (
-      final: prev: {
-        # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/al/alejandra/package.nix
-        # https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
-        # FIXME: Provide a better flake.nix over there instead.
-        alejandra = prev.rustPlatform.buildRustPackage {
-          pname = "alejandra";
-          version = "4.0.0";
-          src = inputs.alejandra-fork;
-          doCheck = false;
-          cargoHash = "sha256-IX4xp8llB7USpS/SSQ9L8+17hQk5nkXFP8NgFKVLqKU=";
-          meta = {
-            license = prev.lib.licenses.unlicense;
-            mainProgram = "alejandra";
-          };
+    alejandra_overlay = final: prev: {
+      # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/al/alejandra/package.nix
+      # https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
+      # FIXME: Provide a better flake.nix over there instead.
+      alejandra = prev.rustPlatform.buildRustPackage {
+        pname = "alejandra";
+        version = "4.0.0";
+        src = inputs.alejandra-fork;
+        doCheck = false;
+        cargoHash = "sha256-IX4xp8llB7USpS/SSQ9L8+17hQk5nkXFP8NgFKVLqKU=";
+        meta = {
+          license = prev.lib.licenses.unlicense;
+          mainProgram = "alejandra";
         };
-      }
-    );
+      };
+    };
 
     # TODO: Use another module system for overlays, and let them depend on hosts
     overlays = import ./overlays ++ [alejandra_overlay];
