da12df3d7a16329f024d8d806e1280d55a0c28e5
Max Dexheimer <maxdexh03@gmail.com>
2025-12-16 11:59:14 +0100

    Typing for condition system

--- options/parts.nix
+++ options/parts.nix
@@ -1,6 +1,6 @@
 {lib, ...}: let
   # TODO: Figure out a way to wrap modules in mkIf
-  part_type = {name, ...}: {
+  partTy = {name, ...}: {
     options = {
       name = lib.mkOption {
         type = lib.types.str;
@@ -20,11 +20,16 @@
       tags = lib.mkOption {
         type = lib.types.listOf lib.types.str;
       };
+      enable = lib.mkOption {
+        type = condTy;
+      };
 
       nixos = lib.mkOption {
         type = lib.types.deferredModule;
         default = {};
       };
+      # FIXME: Allow the same part to have multiple submodules with different conditions
+      # FIXME: This does not give a way to filter by users
       hm = lib.mkOption {
         type = lib.types.deferredModule;
         default = {};
@@ -36,13 +41,80 @@
       };
     };
   };
+
+  condDeclTy = lib.types.attrTag {
+    noneOf = lib.mkOption {
+      type = lib.types.listOf condTy;
+    };
+    oneOf = lib.mkOption {
+      type = lib.types.listOf condTy;
+    };
+    allOf = lib.mkOption {
+      type = lib.types.listOf condTy;
+    };
+    anyOf = lib.mkOption {
+      type = lib.types.listOf condTy;
+    };
+    not = lib.mkOption {
+      type = condTy;
+    };
+
+    tag = lib.mkOption {
+      type = lib.types.submodule {
+        options.name = lib.mkOption {
+          type = lib.types.str; # TODO: Constrain to declared tags
+        };
+      };
+    };
+
+    checks = lib.mkOption {
+      type = lib.types.submodule {
+        host = lib.mkOption {
+          type = lib.uniq (lib.types.functionTo lib.types.bool);
+        };
+        user = lib.mkOption {
+          type = lib.uniq (lib.types.functionTo lib.types.bool);
+        };
+      };
+    };
+
+    hostCheck = lib.mkOption {
+      type = lib.uniq (lib.types.functionTo lib.types.bool);
+    };
+    userCheck = lib.mkOption {
+      type = lib.uniq (lib.types.functionTo lib.types.bool);
+    };
+  };
+
+  condTy = lib.types.either lib.types.bool condDeclTy;
 in {
   options.defaultTags = lib.mkOption {
     type = lib.types.attrsOf (lib.types.nullOr lib.types.bool);
     default = {};
   };
-  options.parts = lib.mkOption {
-    type = lib.types.attrsOf (lib.types.uniq (lib.types.submodule part_type));
-    default = {};
+
+  options = {
+    parts = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.uniq (lib.types.submodule partTy));
+      default = {};
+    };
+
+    tags = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule ({name, ...}: {
+        options = {
+          name = lib.mkOption {
+            readOnly = true;
+            default = name;
+          };
+          default = lib.mkOption {
+            type = lib.types.bool;
+          };
+        };
+      }));
+    };
+
+    conds = lib.mkOption {
+      type = lib.types.attrsOf condDeclTy;
+    };
   };
 }
