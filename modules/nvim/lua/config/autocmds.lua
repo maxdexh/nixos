0555cbd5ddd9fca436bf6f18e947f27a98d0be29
Max Dexheimer <maxdexh03@gmail.com>
2026-02-01 11:22:52 +0100

    Add typst

--- modules/nvim/lua/config/autocmds.lua
+++ modules/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local libs = require("util.libs")
+
+libs.misc.dbg_err(function()
+   -- Thank you LazyVim for just deciding to enable softwrap with an autocommand
+   vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+end)
