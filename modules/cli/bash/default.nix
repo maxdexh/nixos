00183c96ffcdca4ac1088e027a71c3af8a32d2ef
Max Dexheimer <maxdexh03@gmail.com>
2026-05-08 10:18:25 +0200

    Get rid of blesh

--- modules/cli/bash/default.nix
+++ modules/cli/bash/default.nix
@@ -2,23 +2,19 @@
   parts.bash = {
     enableIf.tags.personal = true;
 
-    hm = {
-      host,
-      pkgs,
-      ...
-    }: {
+    hm = {...}: {
       # FIXME: Fix performance issues
       programs.bash = {
         enable = true;
         # Make sure this comes before everything, including e.g. shellAliases
-        bashrcExtra = "[[ $- == *i* ]] && source ${pkgs.blesh}/share/blesh/ble.sh";
-        initExtra = "source ${host.mkNixConfigSymlink ./bashrc-extra.bash}";
+        # bashrcExtra = "[[ $- == *i* ]] && source ${pkgs.blesh}/share/blesh/ble.sh";
+        # initExtra = "source ${host.mkNixConfigSymlink ./bashrc-extra.bash}";
         historyFile = "$XDG_STATE_HOME/bash/history";
-        shellOptions = ["histappend" "checkwinsize" "extglob" "globstar" "checkjobs"];
+        shellOptions = ["histappend" "checkwinsize" "extglob" "globstar" "nullglob" "checkjobs"];
         historyControl = ["ignoreboth"];
       };
 
-      xdg.configFile."blesh/init.sh".source = host.mkNixConfigSymlink ./blerc.bash;
+      # xdg.configFile."blesh/init.sh".source = host.mkNixConfigSymlink ./blerc.bash;
     };
   };
 }
