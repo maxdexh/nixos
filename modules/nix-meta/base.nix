20cae2f7a3b10831dcec8dbbd67bf717ae5f2d87
Max Dexheimer <maxdexh03@gmail.com>
2026-06-09 12:55:38 +0200

    Fix nix registry hack

--- modules/nix-meta/base.nix
+++ modules/nix-meta/base.nix
@@ -4,7 +4,8 @@
 
     hm = {
       host,
-      pkgs,
+      config,
+      lib,
       ...
     }: {
       # NOTE: This currently does nothing when installing hm as a nixos module.
@@ -15,36 +16,44 @@
 
       # Replace nixpkgs with this flake in commands like `nix profile install nixpkgs#package`
       # Also adds an alias so we can use `n#package`
-      nix.registry = assert inputs.self?packages; rec {
-        nixpkgs.to = n.to;
+      nix.registry = {
         n.to = {
-          type = "path";
-          path = toString pkgs.path;
-        };
-        n.from = {
           type = "indirect";
-          id = "n";
+          id = "nixpkgs";
+        };
+        # mimic nixpkgs.flake.source (not available in hm)
+        nixpkgs = lib.mkIf (!host.nixos.enable) {
+          nixpkgs = {
+            exact = true;
+            flake = null;
+            from = {
+              id = "nixpkgs";
+              type = "indirect";
+            };
+            to = {
+              path = "${inputs.self}";
+              type = "path";
+            };
+          };
         };
       };
+      # mimic nixpkgs.flake.source (not available in hm)
+      nix.nixPath = lib.mkIf (!host.nixos.enable) [
+        "nixpkgs=flake:nixpkgs"
+      ];
     };
 
     nixos = {
       config,
       host,
+      lib,
       ...
     }: {
       nix.settings.trusted-users = map (user: config.users.users.${user.username}.name) (builtins.attrValues host.users);
 
+      nixpkgs.flake.source = assert inputs.self?packages; lib.mkForce "${inputs.self}";
+
       nix.channel.enable = false;
-
-      # TODO: Set via hm if not on nixos
-      nix.nixPath = [
-        "nixpkgs=flake:${inputs.nixpkgs}"
-        # FIXME: Make this work
-        # "nixpkgs-overlays=${../overlays/default.nix}"
-
-        # NOTE: Do not set nixos-config, since tools like nixos-option assume it is non-flake
-      ];
     };
   };
 }
